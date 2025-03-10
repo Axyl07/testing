52c5389ba688f325d6017bd50bf553e407b55aea
Axyl07 <aryanchauhan000777@gmail.com>
2025-03-10 14:42:07 +0530

    Calculator done

--- code.js
+++ code.js
@@ -2,14 +2,22 @@
 //     return a + b;
 //   }
 
-export function capitalize(string){
+ function capitalize(string){
   let newString = string[0].toUpperCase() + string.slice(1);
   return newString;
 }
 
-export function reverse(string) {
+ function reverse(string) {
    let array = string.split('')
    array.reverse()
    let result = array.join('')
   return result
 }
+
+let calc = {
+   add : (x, y)=> x + y ,
+  subtract: (x, y) => x - y,
+  divide: (x, y) => x / y,
+  multiply: (x, y) => x * y,
+}
+export { capitalize,reverse,calc };
--- code.test.js
+++ code.test.js
@@ -1,4 +1,4 @@
-import { sum, capitalize,reverse } from "./code.js";
+import { sum, capitalize,reverse,calc } from "./code.js";
 //capitalize
 test("capitalize the first letter of string", () => {
   expect(capitalize("string")).toBe("String");
@@ -23,7 +23,15 @@ test("reverse the given string", () => {
   expect(reverse('bat')).toBe('tab');
 });
 
-
+//calculate
+test('Perform calculations', () => {
+  const num1 = 10;
+  const num2 = 2;
+  expect(calc.add(num1,num2)).toBe(12)
+  expect(calc.subtract(num1,num2)).toBe(8)
+  expect(calc.divide(num1,num2)).toBe(5)
+  expect(calc.multiply(num1,num2)).toBe(20)
+})
 // test('adds 1 + 2 to equal 3', () => {
 //   expect(sum(1, 2)).toBe(3);
 // });
