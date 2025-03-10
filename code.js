50c67ec7b84a6cfcb03e9599fd6904c3db30b4a4
Axyl07 <aryanchauhan000777@gmail.com>
2025-03-10 14:24:42 +0530

    Reverse string done

--- code.js
+++ code.js
@@ -6,3 +6,10 @@ export function capitalize(string){
   let newString = string[0].toUpperCase() + string.slice(1);
   return newString;
 }
+
+export function reverse(string) {
+   let array = string.split('')
+   array.reverse()
+   let result = array.join('')
+  return result
+}
--- code.test.js
+++ code.test.js
@@ -1,4 +1,4 @@
-import { sum, capitalize } from "./code.js";
+import { sum, capitalize,reverse } from "./code.js";
 //capitalize
 test("capitalize the first letter of string", () => {
   expect(capitalize("string")).toBe("String");
@@ -10,6 +10,18 @@ test("capitalize the first letter of string", () => {
   expect(capitalize("power")).toBe("Power");
 });
 
+
+//reverseString
+
+test("reverse the given string", () => {
+  expect(reverse('car')).toBe('rac');
+});
+test("reverse the given string", () => {
+  expect(reverse('cat')).toBe('tac');
+});
+test("reverse the given string", () => {
+  expect(reverse('bat')).toBe('tab');
+});
 
 
 // test('adds 1 + 2 to equal 3', () => {
