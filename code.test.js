8e6d5457ee0f9deda0e417d102471f1a50007cf2
Axyl07 <aryanchauhan000777@gmail.com>
2025-03-10 14:06:32 +0530

    Change to have different test cases for each condition

--- code.test.js
+++ code.test.js
@@ -1,11 +1,15 @@
-
-import {sum,capitalize} from './code.js'
-
-test('capitalize the first letter of string',()=>{
-  expect(capitalize('string')).toBe('String');
-  expect(capitalize('him')).toBe('Him');
-  expect(capitalize('power')).toBe('Power');
+import { sum, capitalize } from "./code.js";
+//capitalize
+test("capitalize the first letter of string", () => {
+  expect(capitalize("string")).toBe("String");
 });
+test("capitalize the first letter of string", () => {
+  expect(capitalize("him")).toBe("Him");
+});
+test("capitalize the first letter of string", () => {
+  expect(capitalize("power")).toBe("Power");
+});
+
 
 
 // test('adds 1 + 2 to equal 3', () => {
